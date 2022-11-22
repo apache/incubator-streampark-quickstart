49bb196033d1b12f038ee978de916b976a2b15b5
benjobs <benjobs@apache.org>
2022-11-22 22:09:25 +0800

    test case add

--- quickstart-flink/quickstart-datastream/datastream_1.16/src/main/scala/org/apache/streampark/flink/quickstart/datastream/QuickStartApp.scala
+++ quickstart-flink/quickstart-datastream/datastream_1.16/src/main/scala/org/apache/streampark/flink/quickstart/datastream/QuickStartApp.scala
@@ -35,37 +35,22 @@ object QuickStartApp extends FlinkStreaming {
    *
    * <strong>2. 只要年龄小于30岁的数据</strong>
    *
-   * <strong>3. kafka数据格式如下:</strong>
-   * {
-   * "name": "benjobs",
-   * "age":  "28",
-   * "gender":   "1",
-   * "address":  "beijing"
-   * }
-   *
-   * <strong>4. mysql表DDL如下:</strong>
-   * create table user(
-   * `name` varchar(32),
-   * `age` int(3),
-   * `gender` int(1),
-   * `address` varchar(255)
-   * )
    */
   override def handle(): Unit = {
+    //1) 从 KAFKA 中读取数据,并过滤
     val source = KafkaSource()
       .getDataStream[String]()
       .map(x => JsonUtils.read[User](x.value))
       .filter(_.age < 30)
 
-    JdbcSink().sink[User](source)(user =>
-      s"""
-         |insert into t_user(`name`,`age`,`gender`,`address`)
-         |value('${user.name}',${user.age},${user.gender},'${user.address}')
-         |""".stripMargin
-    )
+    //2) 写入到 Mysql 中
+    JdbcSink().sink[User](source)(user => {
+      s"insert into t_user(`name`,`age`) value('${user.name}',${user.age})"
+    })
+
   }
 
 }
 
-case class User(name: String, age: Int, gender: Int, address: String)
+case class User(name: String, age: Int)
 
